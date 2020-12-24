c6db28277dbb32020415d25e482773dd78ab9e01
Thiago Martin Poppe <42252349+ThiagoPoppe@users.noreply.github.com>
2020-12-24 11:56:55 -0300

    Removendo comentário com TODO

--- TP1/List.h
+++ TP1/List.h
@@ -21,8 +21,8 @@ void list_append(List* list, int val);
 
 // Funções de remoção de nodes (por índice e por valor)
 // retornando o valor removido
-int remove_by_index(List* list, int idx); // TODO: Corrigir ponteiro tail
-int remove_by_value(List* list, int val); // TODO: Corrigir ponteiro tail
+int remove_by_index(List* list, int idx);
+int remove_by_value(List* list, int val);
 
 // Funções auxiliares (exibir lista e verificar se lista está vazia)
 void show_list(List* list);
