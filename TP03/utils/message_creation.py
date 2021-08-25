ce83908000739e7b096e7b5cb781ea3c3e1c908b
ThiagoPoppe <thiagopoppe@dcc.ufmg.br>
2021-08-25 16:21:08 -0300

    Refatorando criação de mensagens

--- TP03/utils/message_creation.py
+++ TP03/utils/message_creation.py
@@ -3,46 +3,45 @@ from struct import pack
 from .common import type_encoder
 from .constants import SERVER_ID
 
+def create_message_header(message_type, source_id, dest_id, seq_number):
+    """ Função auxiliar para criarmos o cabeçalho das mensagens """
+
+    header = [type_encoder[message_type], source_id, dest_id, seq_number]
+    header = pack('!4H', *header)
+    return header
+
+def send_header_only_message(sock, message_type, source_id, dest_id, seq_number):
+    """ Função auxiliar para enviarmos mensagens que apenas possuem o cabeçalho """
+
+    header = create_message_header(message_type, source_id, dest_id, seq_number)
+    sock.sendall(header)
+
 def send_ok_message(sock, source_id, dest_id, seq_number):
-    message = [type_encoder['OK'], source_id, dest_id, seq_number]
-    message = pack('!4H', *message)
-    sock.sendall(message)
+    send_header_only_message(sock, 'OK', source_id, dest_id, seq_number)
 
 def send_erro_message(sock, source_id, dest_id, seq_number):
-    message = [type_encoder['ERRO'], source_id, dest_id, seq_number]
-    message = pack('!4H', *message)
-    sock.sendall(message)
+    send_header_only_message(sock, 'ERRO', source_id, dest_id, seq_number)
 
 def send_oi_message(sock, source_id, seq_number):
-    message = [type_encoder['OI'], source_id, SERVER_ID, seq_number]
-    message = pack('!4H', *message)
-    sock.sendall(message)
+    send_header_only_message(sock, 'OI', source_id, SERVER_ID, seq_number)
 
 def send_flw_message(sock, source_id, dest_id, seq_number):
-    message = [type_encoder['FLW'], source_id, dest_id, seq_number]
-    message = pack('!4H', *message)
-    sock.sendall(message)
+    send_header_only_message(sock, 'FLW', source_id, dest_id, seq_number)
+
+def send_creq_message(sock, source_id, dest_id, seq_number):
+    send_header_only_message(sock, 'CREQ', source_id, dest_id, seq_number)
 
 def send_msg_message(sock, source_id, dest_id, seq_number, ascii_msg):
     n_bytes = len(ascii_msg)
-
-    header = [type_encoder['MSG'], source_id, dest_id, seq_number]
-    header = pack('!4H', *header)
+    header = create_message_header('MSG', source_id, dest_id, seq_number)
 
     fmt = '!H{}s'.format(n_bytes)
     message = header + pack(fmt, n_bytes, ascii_msg)
     sock.sendall(message)
 
-def send_creq_message(sock, source_id, dest_id, seq_number):
-    message = [type_encoder['CREQ'], source_id, dest_id, seq_number]
-    message = pack('!4H', *message)
-    sock.sendall(message)
-
 def send_clist_message(sock, source_id, dest_id, seq_number, client_list):
     n_clients = len(client_list)
-
-    header = [type_encoder['CLIST'], source_id, dest_id, seq_number]
-    header = pack('!4H', *header)
+    header = create_message_header('CLIST', source_id, dest_id, seq_number)
 
     fmt = '!H{}H'.format(n_clients)
     message = header + pack(fmt, n_clients, *client_list)
@@ -50,9 +49,7 @@ def send_clist_message(sock, source_id, dest_id, seq_number, client_list):
 
 def send_file_message(sock, source_id, dest_id, seq_number, file_id, n_chunks, ascii_ext):
     len_ext = len(ascii_ext)
-
-    header = [type_encoder['FILE'], source_id, dest_id, seq_number]
-    header = pack('!4H', *header)
+    header = create_message_header('FILE', source_id, dest_id, seq_number)
 
     fmt = '!3H{}s'.format(len_ext)
     message = header + pack(fmt, file_id, n_chunks, len_ext, ascii_ext)
@@ -60,9 +57,7 @@ def send_file_message(sock, source_id, dest_id, seq_number, file_id, n_chunks, a
 
 def send_file_chunk_message(sock, source_id, dest_id, seq_number, file_id, chunk_id, chunk):
     len_chunk = len(chunk)
-
-    header = [type_encoder['FILE_CHUNK'], source_id, dest_id, seq_number]
-    header = pack('!4H', *header)
+    header = create_message_header('FILE_CHUNK', source_id, dest_id, seq_number)
 
     fmt = '!3H{}s'.format(len_chunk)
     message = header + pack(fmt, file_id, chunk_id, len_chunk, chunk)
